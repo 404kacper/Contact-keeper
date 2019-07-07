1e51226507d932ff2f7fc69d3ae8dcc29a520c30
KiszonyKacper <38114155+KiszonyKacper@users.noreply.github.com>
2019-07-07 15:42:33 +0200

    Adding & deleting contacts functionality / without db yet

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -16,21 +16,21 @@ const ContactState = props => {
   const initialState = {
     contacts: [
       {
-        _id: 1,
+        id: 1,
         name: "Jill Gill",
         email: "jillG@gmail.com",
         phone: "111-111-1111",
         type: "personal"
       },
       {
-        _id: 2,
+        id: 2,
         name: "Hary Smirk",
         email: "harryMS@gmail.com",
         phone: "222-333-4444",
         type: "personal"
       },
       {
-        _id: 3,
+        id: 3,
         name: "Sara Watson",
         email: "saraW@gmail.com",
         phone: "222-333-5555",
@@ -41,28 +41,38 @@ const ContactState = props => {
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
-  // Add Contact 
+  // Add Contact
+  const addContact = contact => {
+    contact.id = uuid.v4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // Set Current Contact
 
   // Clear Current Contact
 
-  // Update Contact 
+  // Update Contact
 
   // Filter Contacts
 
   // Clear Filter
 
   return (
-      <ContactContext.Provider
+    <ContactContext.Provider
       value={{
-          contacts: state.contacts
-      }}>
-          {props.children}
-      </ContactContext.Provider>
-  )
+        contacts: state.contacts,
+        addContact,
+        deleteContact
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
 };
 
 export default ContactState;
