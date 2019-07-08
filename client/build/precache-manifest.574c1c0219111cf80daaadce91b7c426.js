e34907abc1f1990f277594eb5f846b004586b5f6
KiszonyKacper <38114155+KiszonyKacper@users.noreply.github.com>
2019-07-08 19:32:19 +0200

    New build

---
+++ client/build/precache-manifest.574c1c0219111cf80daaadce91b7c426.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498c04d09bf81f1aa34a2c34e1cb0176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80c68ef2b6a38312120e",
+    "url": "/static/css/main.9a060bc4.chunk.css"
+  },
+  {
+    "revision": "62bfd6d98676a911ba78",
+    "url": "/static/js/2.2bdf8ce5.chunk.js"
+  },
+  {
+    "revision": "80c68ef2b6a38312120e",
+    "url": "/static/js/main.8a3dfe36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
