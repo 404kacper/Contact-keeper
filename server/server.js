459f711d8c00d005bf36dfc78cdd231b47d8ec16
404kacper <kacper.sionkowski@gmail.com>
2023-05-26 17:31:59 +0200

    Installed cors in server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('../config/db');
 const config = require('../config/node_modules/config');
+const cors = require('cors');
 
 const app = express();
 
