f58e547bd6c3c7f8a6d4c4a8de758b7b50ce3a63
404kacper <kacper.sionkowski@gmail.com>
2023-05-26 14:53:53 +0200

    Added check for env variable

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('../config/db');
-const path = require('path');
 
 const app = express();
 
@@ -16,16 +15,17 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+  if (process.env.NODE_ENV == 'production') {
+    console.log(
+      `NODE_ENV is set to: ${process.env.NODE_ENV} using production package.`
+    );
+  } else {
+    console.log(
+      `NODE_ENV wasn't set defaulting to default.json package an dev environment.`
+    );
+  }
+});
